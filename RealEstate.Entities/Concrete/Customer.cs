a860f3871721f5570d89ac4fba3c2540f054b119
Gokhan <engokhangok@gmail.com>
2023-07-20 18:58:19 +0300

    Create Entities

---
+++ RealEstate.Entities/Concrete/Customer.cs
@@ -0,0 +1,10 @@
+﻿namespace RealEstate.Entities.Concrete
+{
+    public class Customer
+    {
+        public int CustomerID { get; set; }
+        public string Name { get; set; }
+        public string City { get; set; }
+    }
+
+}
